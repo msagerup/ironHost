de006b87118208cfc8d6b887ecce7ed58c36c54b
Magnus Sagerup <magnus.sagerup@gmail.com>
2023-10-11 17:51:03 +0200

    added test

--- src/apis/call_request.rs
+++ src/apis/call_request.rs
@@ -6,7 +6,7 @@ use std::env;
 use reqwest::header::{HeaderMap, HeaderValue};
 
 // Call Large Language Model.
-pub async fn call_gtp(messages: Vec<Message>) {
+pub async fn call_gtp(messages: Vec<Message>) -> Result<String, Box<dyn std::error::Error>> {
     dotenv().ok();
 
     // Extract API key info.
@@ -22,24 +22,23 @@ pub async fn call_gtp(messages: Vec<Message>) {
     // Create api key header.
     headers.insert(
         "authorization",
-        HeaderValue::from_str(&format!("Bearer {}", api_key)).unwrap(),
+        HeaderValue::from_str(&format!("Bearer {}", api_key))?
     );
 
     // Create org key header.
     headers.insert(
         "OpenAI-Organization",
-        HeaderValue::from_str(api_org.as_str()).unwrap(),
+        HeaderValue::from_str(api_org.as_str())?
     );
 
     // Call client
     let client: Client = Client::builder()
     .default_headers(headers)
-    .build()
-    .unwrap();
+    .build()?;
 
     // Create chat completion 
     let chat_completion: ChatCompletion  = ChatCompletion {
-        model: "gtp-4".to_string(),
+        model: "gpt-4".to_string(),
         messages,
         temperature: 0.1
     };
@@ -49,10 +48,31 @@ pub async fn call_gtp(messages: Vec<Message>) {
     .post(url)
     .json(&chat_completion)
     .send()
-    .await
-    .unwrap();
+    .await?;
 
-    dbg!(res_raw.text().await.unwrap());
+    let text = res_raw.text().await?;
+
+    dbg!(&text);
+
+    Ok(text)
+    
+}
 
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn tests_call_to_openai() -> Result<(), Box<dyn std::error::Error>> {
+        let message: Message = Message {
+            role: "user".to_string(),
+            content: "Hello how are you?".to_string()
+        };
+        let messages: Vec<Message> = vec!(message);
+        let res_raw: String = call_gtp(messages).await?;
+
+        
+    assert!(!res_raw.contains("error"), "API call returned an error");
+    Ok(())
+    }
 }
